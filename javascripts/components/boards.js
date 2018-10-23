1ee691a6c75a8ffd4aff098e697dbcb24d60b812
Timothy Harley <timothy.d.harley@gmail.com>
2018-10-22 21:43:46 -0500

    finished but im lost

--- javascripts/components/boards.js
+++ javascripts/components/boards.js
@@ -1,23 +1,26 @@
 import {loadBoards} from '../data/boardsData.js';
+import {initialPinView} from '../components/pins.js';
+import {loadPinsOnBoards} from '../data/pinsData.js';
 
 const bindEvents = () => {
     $('#user-boards').on('click', '.board-card', (e) => {
         const clickedBoardId = $(e.target).closest('.board-card').attr('id');
         $('#boards-page').hide();
         $('#pins-page').show();
-        console.log(clickedBoardId)
+        initialPinView(clickedBoardId)
     })
 }
 
 const writeBoards = (boards) => {
     let domString = '';
     boards.forEach(board => {
+        const boardImg = board.pins[0] ? board.pins[0].image_url : './db/default-img.jpeg';
         domString += `
         <div id='${board.id}' class="board-card p-2">
-          <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
+          <img class="card-img-top" src="${boardImg}" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">42 Pins</p>
+            <h5 class="card-title">${board.name}</h5>
+            <p class="card-text">${board.pins.length} Pins</p>
           </div>
       </div>
       `
@@ -27,10 +30,13 @@ const writeBoards = (boards) => {
 
 const initializeBoardView = () => {
     loadBoards().then((boards) => {
-        writeBoards(boards);
+        
+        return loadPinsOnBoards(boards);
+    }).then((boardsWithPins) => {
+        writeBoards(boardsWithPins);
         bindEvents();
     }).catch((error) => {
-        console.error('error')
+        console.error(error);
     })
 }
 
